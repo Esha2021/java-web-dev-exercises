04e0354662c15d6588542cdff3fc6164f32735e4
Esha2021 <eeswaricse@gmail.com>
2021-08-26 20:43:54 -0500

    datatype studio &Exercise

--- src/studios/areaofacircle/Area.java
+++ src/studios/areaofacircle/Area.java
@@ -2,26 +2,33 @@ package studios.areaofacircle;
 
 import java.util.Scanner;
 public class Area {
-    public static void main(String[] args){
-         double radius;
-         double pi=3.14;
-         Scanner input;
-           input = new Scanner(System.in);
-           System.out.println("Enter the radius:");
-           radius= input.nextFloat();
-           input.close();
+    public static void main(String[] args) {
+        double radius;
+        double pi = 3.14;
+        Scanner input;
+        input = new Scanner(System.in);
+        //System.out.println("Enter the radius:");
+        //radius= input.nextFloat();
+        //input.close();
+        do {
+            System.out.println("Enter the radius:");
+            if (input.hasNextDouble()) {
+                //System.out.println("Enter the radius:");
+                radius = input.nextDouble();
 
-           if(radius>0) {
-               double area = circle.getArea(radius);
-               System.out.println("The area of a circle of radius " + radius + " is: " + area);
-           } else if(radius<0 ){
-               System.out.println("Enter positive number");
-           }else
-        {
-            System.out.println("Enter positive number");
-        }
+                if (radius > 0) {
+                    double area = circle.getArea(radius);
+                    System.out.println("The area of a circle of radius " + radius + " is: " + area);
+               } //else if (radius < 0) {
+//                    System.out.println("Enter positive number");
+//                }
+            } else {
+                radius=-1;
+                System.out.println("please Enter the number");
+                 input.next();
+               //System.out.println("Enter  number");
+            }
 
-
+        }while(radius<=0);
     }
-
 }
